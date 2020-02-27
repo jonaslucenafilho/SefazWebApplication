ca81378a9de80fb3d5bff5cd74e15d35ac6c8b8d
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-27 20:11:11 -0300

    creation of the authentication filter class for login

---
+++ SefazWebApplication/src/main/java/br/com/sefaz/filter/AutenticationFilter.java
@@ -0,0 +1,54 @@
+package br.com.sefaz.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.sefaz.jpautil.JPAUtil;
+import br.com.sefaz.model.User;
+
+@WebFilter(urlPatterns = { "/*" })
+public class AutenticationFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+
+		User loggedUser = (User) session.getAttribute("loggedUser");
+
+		String url = req.getServletPath();
+
+		if (!url.equalsIgnoreCase("index.jsf") && loggedUser == null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsf");
+			dispatcher.forward(request, response);
+			return;
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		JPAUtil.getEntityManager();
+
+	}
+
+}
