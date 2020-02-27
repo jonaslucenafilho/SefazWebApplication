7191667cb0fd91ea9b7f560b5392adf91eea0167
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-27 20:15:49 -0300

    implementation of the logInto method for redirecting the authenticated user

--- SefazWebApplication/src/main/java/br/com/sefaz/controller/UserBean.java
+++ SefazWebApplication/src/main/java/br/com/sefaz/controller/UserBean.java
@@ -1,53 +1,31 @@
 package br.com.sefaz.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import br.com.sefaz.dao.DaoGeneric;
 import br.com.sefaz.model.User;
+import br.com.sefaz.repository.IDaoUser;
+import br.com.sefaz.repository.IDaoUserImpl;
 
 @ViewScoped
 @ManagedBean(name = "userBean")
-public class UserBean {
+public class UserBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private User user = new User();
 	private DaoGeneric<User> daoGeneric = new DaoGeneric<User>();
 	private List<User> users = new ArrayList<User>();
 	
-//	public String save() {
-//		daoGeneric.save(user);
-//		
-//		return "";
-//	}
-	
-	public String saveUser() {
-		user = daoGeneric.update(user);
-		user = new User();
-		loadUsers();
-		return "";
-	}
-	
-	public String newUser() {
-		user = new User();
-		return "";
-	}
-	
-	public String deleteUser() {
-		daoGeneric.delete(user);
-		user = new User();
-		loadUsers();
-		return "";
-	}
-	
-	@PostConstruct
-	public void loadUsers() {
-		users = daoGeneric.getListEntity(User.class);
-	}
-	
+	private IDaoUser iDaoUser = new IDaoUserImpl();
 
 	public User getUser() {
 		return user;
@@ -64,11 +42,55 @@ public class UserBean {
 	public void setDaoGeneric(DaoGeneric<User> daoGeneric) {
 		this.daoGeneric = daoGeneric;
 	}
-	
+
 	public List<User> getUsers() {
 		return users;
 	}
-	
-	
-}
 
+//	public String save() {
+//		daoGeneric.save(user);
+//		
+//		return "";
+//	}
+
+	public String saveUser() {
+		user = daoGeneric.update(user);
+		user = new User();
+		loadUsers();
+		return "";
+	}
+
+	public String newUser() {
+		user = new User();
+		return "";
+	}
+
+	public String deleteUser() {
+		daoGeneric.delete(user);
+		user = new User();
+		loadUsers();
+		return "";
+	}
+
+	@PostConstruct
+	public void loadUsers() {
+		users = daoGeneric.getListEntity(User.class);
+	}
+	
+	public String logInto() {
+
+		User userLogged = iDaoUser.consultUser(user.getEmail(), user.getPassword());
+
+		if (userLogged != null) {
+
+			FacesContext context = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = context.getExternalContext();
+			externalContext.getSessionMap().put("loggedUser", userLogged);
+
+			return "firstpage.jsf";
+		}
+
+		return "index.jsf";
+	}
+
+}
