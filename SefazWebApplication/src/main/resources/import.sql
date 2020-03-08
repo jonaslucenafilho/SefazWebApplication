050cde97e7509dbd7a792f2d4712edfe3f9028d9
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-03-08 16:52:37 -0300

    Version 1.0.4

--- SefazWebApplication/src/main/resources/import.sql
+++ SefazWebApplication/src/main/resources/import.sql
@@ -1,1 +1,30 @@
 INSERT INTO USERS (id,email,name,password) VALUES (1, 'admin@admin.com', 'admin', 'admin');
+INSERT INTO USERS (id,email,name,password) VALUES (2, 'jonas@gmail.com', 'Jonas Cavalcante de Lucena', 'jonas');
+INSERT INTO USERS (id,email,name,password) VALUES (3, 'joao@hotmail.com', 'João Antônido da Silva', 'joao');
+INSERT INTO USERS (id,email,name,password) VALUES (4, 'marcos@yahoo.com', 'Marcos Guilherme dos Santos', 'marcos');
+INSERT INTO USERS (id,email,name,password) VALUES (5, 'adriano@globo.com', 'Adriano Batista Silveira', 'adriano');
+INSERT INTO USERS (id,email,name,password) VALUES (6, 'maria@gmail.com', 'Maria Aparecida da Silva', 'maria');
+INSERT INTO USERS (id,email,name,password) VALUES (7, 'marina@hotmail.com', 'Marina Andrade da Boa Hora', 'marina');
+INSERT INTO USERS (id,email,name,password) VALUES (8, 'rafaela@gmail.com', 'Rafaela Albuquerque Oliveira', 'rafaela');
+INSERT INTO USERS (id,email,name,password) VALUES (9, 'mariana@hotmail.com', 'Mariana Faraco Coelho', 'mariana');
+INSERT INTO USERS (id,email,name,password) VALUES (10, 'isabela@yahoo.com', 'Isabela Drummond de Andrade', 'isabela');
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (1, 81, '9999-9999', 'Pessoal', 1)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (2, 81, '9888-8888', 'Comercial', 1)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (3, 81, '9999-9999', 'Pessoal', 2)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (4, 81, '9888-8888', 'Comercial', 2)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (5, 81, '9999-9999', 'Pessoal', 3)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (6, 81, '9888-8888', 'Comercial', 3)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (7, 81, '9999-9999', 'Pessoal', 4)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (8, 81, '9888-8888', 'Comercial', 4)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (9, 81, '9999-9999', 'Pessoal', 5)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (10, 81, '9888-8888', 'Comercial', 5)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (11, 81, '9999-9999', 'Pessoal', 6)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (12, 81, '9888-8888', 'Comercial', 6)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (13, 81, '9999-9999', 'Pessoal', 7)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (14, 81, '9888-8888', 'Comercial', 7)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (15, 81, '9999-9999', 'Pessoal', 8)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (16, 81, '9888-8888', 'Comercial', 8)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (17, 81, '9999-9999', 'Pessoal', 9)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (18, 81, '9888-8888', 'Comercial', 9)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (19, 81, '9999-9999', 'Pessoal', 10)
+INSERT INTO PHONES (id,ddd,number,type,user_id) VALUES (20, 81, '9888-8888', 'Comercial', 10)
